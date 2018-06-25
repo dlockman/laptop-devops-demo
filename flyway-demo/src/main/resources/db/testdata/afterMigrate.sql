dc0723549b0a10fb46eaeb9d17096162b357571c
marcosmercuri <marcosmercuri@gmail.com>
2018-06-25 15:47:36 +0200

    Add flyway demo project

---
+++ flyway-demo/src/main/resources/db/testdata/afterMigrate.sql
@@ -0,0 +1,5 @@
+-- Make sure the script is idempotent
+-- or use language tools like the ON CONFLICT clause in postgresql.
+insert into PERSON (ID, NAME) values (1, 'Axel');
+insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
+insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
